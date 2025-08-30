274d89c330425f12fb57c6d2354066edbae4b6b0
NITHINKR06 <nithinpoojari606@gmail.com>
2025-08-30 21:21:50 +0530

    `Removed isNotFoundPage condition from Layout component and AdminAuthProvider`

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -21,7 +21,7 @@ const Layout = () => {
       <ParticlesBackground />
       
       {/* GlitchText Background - Only on non-404 pages */}
-      {/* {!isNotFoundPage && (
+      {!isNotFoundPage && (
         <div className="fixed inset-0 flex items-center justify-center pointer-events-none z-0">
           <GlitchText
             speed={3}
@@ -32,7 +32,7 @@ const Layout = () => {
             CSI
           </GlitchText>
         </div>
-      )} */}
+      )}
       
       {/* Navigation - Hidden on 404 */}
       {!isNotFoundPage && <Navbar />}
